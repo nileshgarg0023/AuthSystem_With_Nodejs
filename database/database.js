6de83d0a053f35c91a90a0cc04a1cd39fea18f37
Nilesh Garg <45899979+nileshgarg0023@users.noreply.github.com>
2023-02-11 06:18:18 +0000

    added db url

--- database/database.js
+++ database/database.js
@@ -1,1 +1,18 @@
 const mongoose = require('mongoose');
+
+const MONGODB_URL = process.env;
+
+// db password = kj6DY3Ciw1J3pZk4
+//db username = authAdmin
+exports.connect = () =>{
+    mongoose.connect(MONGODB_URL,{
+        useNewUrlParser:true,
+        useUnifiedTopology:true,
+    })
+        .then()
+        .catch((error) => {
+            console.log(`DB connection FAILED`);
+            console.log(error);
+            process.exit(1);
+        });
+}
