b46974fbe7999906258c731f601d0bd0bcd01c1f
Nilesh Garg <45899979+nileshgarg0023@users.noreply.github.com>
2023-02-11 07:00:27 +0000

    connected and tested DB

--- database/database.js
+++ database/database.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose")
 
-const MONGODB_URL = process.env
+const {MONGODB_URL} = process.env
 
 exports.connect = () => {
     mongoose.connect(MONGODB_URL, {
-        useNewUrlParser: true,
+        useNewUrlParser: true, 
         useUnifiedTopology: true
     })
     .then()
