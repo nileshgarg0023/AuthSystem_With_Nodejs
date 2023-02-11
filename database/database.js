06cb97781d7847b835aec566bb2c64ec78320d69
Nilesh Garg <45899979+nileshgarg0023@users.noreply.github.com>
2023-02-11 06:19:51 +0000

    removed admin password from the file

--- database/database.js
+++ database/database.js
@@ -2,8 +2,6 @@ const mongoose = require('mongoose');
 
 const MONGODB_URL = process.env;
 
-// db password = kj6DY3Ciw1J3pZk4
-//db username = authAdmin
 exports.connect = () =>{
     mongoose.connect(MONGODB_URL,{
         useNewUrlParser:true,
